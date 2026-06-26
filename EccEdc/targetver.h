2c16165edd0d3b943d960ecc2c8ee2127a4a765b
shfil <shfil@paranoici.org>
2026-06-26 03:25:59 +0200

    fix

--- EccEdc/targetver.h
+++ EccEdc/targetver.h
@@ -5,4 +5,16 @@
 // 以前の Windows プラットフォーム用にアプリケーションをビルドする場合は、WinSDKVer.h をインクルードし、
 // SDKDDKVer.h をインクルードする前に、サポート対象とするプラットフォームを示すように _WIN32_WINNT マクロを設定します。
 
+#ifdef _MSC_VER
+// If compiling with Microsoft Visual Studio, use the official SDK header
 #include <SDKDDKVer.h>
+#else
+// Alternative for MinGW / GCC / Clang on Linux:
+// Define target Windows version manually (0x0A00 targets Windows 10 and Windows 11)
+#ifndef WINVER
+#define WINVER 0x0A00
+#endif
+#ifndef _WIN32_WINNT
+#define _WIN32_WINNT 0x0A00
+#endif
+#endif
